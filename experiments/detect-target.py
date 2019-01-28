d073cf322bfa666e5fe2094220cbcedc4eec4457
Mikhail Kyraha <mikhail@kyraha.com>
2019-01-27 19:05:49 -0600

    Added comments in the target detector code

--- experiments/detect-target.py
+++ experiments/detect-target.py
@@ -17,6 +17,8 @@ def normalize_rect(rect):
     # rect[1] is tuple of the sizes (x, y)
     # rect[2] is the tilt angle
     if rect[1][0] > rect[1][1]:
+        # if x > y then we turn it 90 degrees
+        # which means swap the sizes and turn cw or ccw depending on the previous value
         new_angle = rect[2] + 90.0 if rect[2] < 0.0 else -90.0
         return (
             rect[0],                    # same coordinates
@@ -31,35 +33,43 @@ if len(sys.argv) < 2:
     print "Usage: {} image-file.png".format(sys.argv[0])
     sys.exit("No file name is given")
 
-# Read the image directly from the file into the frame object
+# read the image directly from the file into the frame object
 frame = cv2.imread(sys.argv[1])
+# we want csv color space because it's better suited for color filtering
 hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-# These are two arrays for lower and upper boundaries of the color range
+# these are two arrays for lower and upper boundaries of the color range
+# get these "magic" numbers by running the "color-filter" experiment
 minHSV = np.array([67, 150, 113])
 maxHSV = np.array([81, 255, 255])
 
-# create a bitmap based on the new threshold values
+# create a bitmap based on the threshold values
 mask_hsv = cv2.inRange(hsv, minHSV, maxHSV)
-# find contours if the highlighted areas in the bitmap
+# find contours of the areas in the bitmap
 contours, hierarchy = cv2.findContours(mask_hsv, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
 # find image moments for each contour
 moments = ([cv2.moments(x) for x in contours])
 # find Hu moments for each contour
 hu_moments = ([cv2.HuMoments(x) for x in moments])
+# dunno what to do with these moments yet, just print them out for now
 for a_moment in hu_moments:
     print a_moment
 
-# find bounding rectangles around the contours to estimate their tilt
+# find bounding rectangles around the contours and normalize them
+# using our custom normalize_rect function to estimate their tilt
 rectangles = ([normalize_rect(cv2.minAreaRect(x)) for x in contours])
 
 # draw things on the original image
 for rect in rectangles:
+    # let's make right leaning rectangles pink and the left - yellow
     color = (255, 0, 255) if rect[2] > 0 else (0, 255, 255)
+    # convert each rectangle into boxes and then contours before drawing
     display = cv2.drawContours(frame, [np.int0(cv2.boxPoints(rect))], -1, color, 3)
 
+# show the result in a window
 cv2.imshow('image', display)
-# Wait indefinitely for a key pressed
+# wait indefinitely for a key pressed
 cv2.waitKey(0)
 # Clean up and quit
 cv2.destroyAllWindows()
